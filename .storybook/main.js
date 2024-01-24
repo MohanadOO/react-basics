97e0620009fbe629673e72a730d1ef90caffeec2
MohanadOO <moohand220@gmail.com>
2024-01-24 16:22:39 +0300

    feat: ✨ Add RTL addon to test components

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,7 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    'storybook-addon-rtl',
   ],
   framework: {
     name: '@storybook/react-webpack5',
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,8 @@
 import '../src/styles/storybook.css';
 
+import { initializeRTL } from 'storybook-addon-rtl';
+initializeRTL();
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
